a250bbc9bac3ace42fe38700ff4c0efb8d022e98
BenBernardCIS <benbernard@cliffislandsoftware.com>
2019-01-28 12:06:20 -0500

    Adding shells of both files with explanations in the comments.

---
+++ src/main/java/org/frc5687/deepspace/robot/Constants.java
@@ -0,0 +1,19 @@
+package org.frc5687.deepspace.robot;
+
+public class Constants {
+    public static final int CYCLES_PER_SECOND = 50;
+
+    /*
+     There should be a nested static class for each subsystem and for each autonomous command that needs tuning constants.
+     For example:
+    public static class DriveTrain {
+        public static final double DEADBAND = 0.3;
+        public static final double SENSITIVITY_LOW_GEAR = 0.8;
+        public static final double SENSITIVITY_HIGH_GEAR = 1.0;
+        public static final double ROTATION_SENSITIVITY = 1.0;
+        public static final double ROTATION_SENSITIVITY_HIGH_GEAR = 1.0;
+        public static final double ROTATION_SENSITIVITY_LOW_GEAR = 0.8;
+    }
+     */
+
+}

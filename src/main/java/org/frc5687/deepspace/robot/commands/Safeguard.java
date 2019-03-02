aead8e483088436b3bf8d556508f7b116ebe93aa
Ben Bernard <benbernard@cliffislandsoftware.com>
2019-03-02 11:55:45 -0500

    Pre-finals changes.

---
+++ src/main/java/org/frc5687/deepspace/robot/commands/Safeguard.java
@@ -0,0 +1,31 @@
+package org.frc5687.deepspace.robot.commands;
+
+import org.frc5687.deepspace.robot.Robot;
+import org.frc5687.deepspace.robot.commands.intake.GripClaw;
+
+public class Intake extends OutliersCommand{
+    private Robot _robot;
+
+    public Intake(Robot robot) {
+        _robot = robot;
+    }
+
+    @Override
+    protected void initialize() {
+        if (_robot.getConfiguration() == Robot.Configuration.hatch) {
+            (new GripClaw(_robot.getHatchIntake())).start();
+        } else if (_robot.getConfiguration() == Robot.Configuration.cargo) {
+            (new IntakeCargo(_robot)).start();
+        }
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+}
+

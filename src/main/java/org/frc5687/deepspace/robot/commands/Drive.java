a63a14aab149d4947c36ee80f34fa8256c2a661d
BenBernardCIS <benbernard@cliffislandsoftware.com>
2019-03-08 09:13:17 -0500

    Added outline of auto-align to Drive, cleaned up IML.

--- src/main/java/org/frc5687/deepspace/robot/commands/Drive.java
+++ src/main/java/org/frc5687/deepspace/robot/commands/Drive.java
@@ -8,10 +8,19 @@ public class Drive extends OutliersCommand {
     private OI _oi;
     private DriveTrain _driveTrain;
 
+    private boolean _autoAlignEnabled = false;
+
     public Drive(DriveTrain driveTrain, OI oi) {
         _driveTrain = driveTrain;
         _oi = oi;
         requires(_driveTrain);
+    }
+
+
+
+    @Override
+    protected void initialize() {
+        // create the _angleController here, just like in AutoDriveToTarget
     }
 
     @Override
@@ -23,6 +32,19 @@ public class Drive extends OutliersCommand {
         // Get the rotation from the tiller
         double wheelRotation = _oi.getDriveRotation();
 
+        // If the auto-align trigger is pressed, and !_autoAlignEnabled:
+        //   Enable the LEDs
+        // else if auto_align trigger is not pressed, and _autoAlignEnabled
+        //   disable the LEDs, disable the controller
+        // else if _autoAlignEnabled
+        //   Get target info (copy from AutoAlignToTarget)
+        //   If target sighted and ither controller not enabled or new setpoint different enough from old setpoint
+        //      set setPoint
+        //      enable controller
+
+
+
+        // If autoAlignEnabled and pidControllerEnabled, send pidOut in place of wheelRotation (you may need a scale override flag as discussed earlier)
         _driveTrain.cheesyDrive(stickSpeed, wheelRotation, _oi.isCreepPressed());
     }
 

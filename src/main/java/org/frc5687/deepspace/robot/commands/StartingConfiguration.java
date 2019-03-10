90b6b83fc28da31b905d7bac0a48e1ac3af3ff1d
BenBernardCIS <benbernard@post.harvard.edu>
2019-03-10 11:17:46 -0400

    Adjusting elevator speed down during sandstorm pickup.

--- src/main/java/org/frc5687/deepspace/robot/commands/StartingConfiguration.java
+++ src/main/java/org/frc5687/deepspace/robot/commands/StartingConfiguration.java
@@ -2,7 +2,6 @@ package org.frc5687.deepspace.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.frc5687.deepspace.robot.Robot;
-import org.frc5687.deepspace.robot.commands.intake.CargoIntakeDown;
 import org.frc5687.deepspace.robot.commands.intake.CargoIntakeUp;
 import org.frc5687.deepspace.robot.commands.intake.ClawWristDown;
 import org.frc5687.deepspace.robot.commands.intake.PointClaw;
@@ -10,11 +9,11 @@ import org.frc5687.deepspace.robot.subsystems.Elevator;
 
 public class StartingConfiguration extends CommandGroup {
     public StartingConfiguration(Robot robot) {
-        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.ClearRoller, Elevator.MotionMode.Ramp, null));
+        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.ClearRoller, Elevator.MotionMode.Ramp, null, 0.0));
         addSequential(new ClawWristDown(robot));
         addSequential(new PointClaw(robot.getHatchIntake()));
         addSequential(new CargoIntakeUp(robot.getCargoIntake()));
-        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Hatch1, Elevator.MotionMode.Ramp, null));
+        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Hatch1, Elevator.MotionMode.Ramp, null, 0.0));
         addSequential(new SetConfiguration(robot, Robot.Configuration.starting));
     }
 }

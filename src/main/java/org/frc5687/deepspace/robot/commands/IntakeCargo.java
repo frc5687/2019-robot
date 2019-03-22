bd9e5fb6067ac67c0c5d629b0f6f006a8572c6ef
BenBernardCIS <benbernard@post.harvard.edu>
2019-03-21 22:28:03 -0400

    Changing IntakeCargo so elevator doesn't overshoot.

--- src/main/java/org/frc5687/deepspace/robot/commands/IntakeCargo.java
+++ src/main/java/org/frc5687/deepspace/robot/commands/IntakeCargo.java
@@ -1,6 +1,7 @@
 package org.frc5687.deepspace.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.frc5687.deepspace.robot.Constants;
 import org.frc5687.deepspace.robot.Robot;
 import org.frc5687.deepspace.robot.commands.intake.*;
 import org.frc5687.deepspace.robot.subsystems.Elevator;
@@ -8,7 +9,7 @@ import org.frc5687.deepspace.robot.subsystems.Elevator;
 public class IntakeCargo extends CommandGroup {
 
     public IntakeCargo(Robot robot) {
-        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Bottom, Elevator.MotionMode.Ramp, null, 0.0));
+        addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Bottom, Elevator.MotionMode.Simple, null, Constants.Elevator.MODE_SPEED));
         addSequential(new StartRoller(robot.getCargoIntake(), true));
         addSequential(new StopRoller(robot.getCargoIntake()));
         addSequential(new CargoIntakeUp(robot.getCargoIntake()));

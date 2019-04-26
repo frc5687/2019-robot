2c5648c34db47823fb99a24dc5a81513d15ad8b4
Dennis_Slobodzain <dennis.slobodzian@baxter-academy.org>
2019-04-26 11:21:51 -0400

    two hatch

--- src/main/java/org/frc5687/deepspace/robot/commands/drive/TwoHatchRocket.java
+++ src/main/java/org/frc5687/deepspace/robot/commands/drive/TwoHatchRocket.java
@@ -36,19 +36,20 @@ public class TwoHatchRocket extends CommandGroup {
             addSequential(new AutoLaunch(robot));
         }
         addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), left ? -30 : 30, 1, 1000, 2, "aligning to rocket"));
-        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .5, false, 0));
+        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .7, false, 0));
         addSequential(new PointClaw(robot.getHatchIntake()));
-        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -12, 1,true, true, 0, "reverse 12 inches", 1000));
+        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -12, .5, false,true, 0, "reverse 12 inches", 1000));
         addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), left ? -170 : 170, 1, 1000, 2, "aligning to rocket"));
-        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .5, false, 0));
+        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .7, false, 0));
         addSequential(new GripClaw(robot.getHatchIntake()));
-        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -12, 1,true, true, 0, "reverse 12 inches", 1000));
+        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -12, .5,false, true, 0, "reverse 12 inches", 1000));
         addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), left ? -170 : 170, 1, 1000, 2, "aligning to rocket"));
-        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -30, 1,true, true, 0, "reverse 12 inches", 1000));
+        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -45, .5,false, true, 0, "reverse 12 inches", 1000));
         addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), left ? -30 : 30, 1, 1000, 2, "aligning to rocket"));
         addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Hatch3, Elevator.MotionMode.Ramp, robot.getOI(), 0.0));
-        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .5, false, 0));
-        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -6, 1,true, true, 0, "reverse 12 inches", 1000));
+        addSequential(new AutoDriveToTargetSimple(robot.getDriveTrain(), robot.getIMU(), robot.getOI(), robot.getLimelight(), robot.getElevator(), robot.getCargoIntake(), robot.getHatchIntake(), robot.getPoseTracker(), .6, false, 0));
+        addSequential(new PointClaw(robot.getHatchIntake()));
+        addSequential(new AutoDrive(robot.getDriveTrain(), robot.getIMU(), robot.getHatchIntake(), robot.getElevator(), -12, .5,false, true, 0, "reverse 12 inches", 1000));
         addSequential(new MoveElevatorToSetPoint(robot.getElevator(), Elevator.Setpoint.Bottom, Elevator.MotionMode.Ramp, robot.getOI(), 0.0));
         addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), left ? -170 : 170, 1, 1000, 2, "aligning to rocket"));
 

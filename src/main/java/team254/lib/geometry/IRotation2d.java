3de4d90ff5a0dae0e0f0c22b9d6f9fc150654d8f
Dennis_Slobodzain <dennis.slobodzian@baxter-academy.org>
2019-05-29 11:20:54 -0400

    no message

--- src/main/java/team254/lib/geometry/IRotation2d.java
+++
@@ -1,5 +0,0 @@
-package team254.lib.geometry;
-
-public interface IRotation2d<S> extends State<S> {
-    public Rotation2d getRotation();
-}
--- src/main/java/team254/lib/util/CSVWritable.java
+++
@@ -1,5 +0,0 @@
-package team254.lib.util;
-
-public interface CSVWritable {
-    String toCSV();
-}
